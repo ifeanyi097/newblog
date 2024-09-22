195992d71bf7d57efe0696fa02dcb8176f9cd70e
ifeanyi <ifeanyiogu097@gmail.com>
2024-09-22 07:55:02 +0000

    added title and date to models

--- app/models.py
+++ app/models.py
@@ -3,13 +3,17 @@ from django.db import models
 
 class Content(models.Model):
     description = models.TextField()
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'content -{self.description}'
+        f'file content -{self.title}'
+
 
 class Gallery(models.Model):
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
     file = models.FileField(upload_to='files')
 
     def __str__(self):
-        return 'file for content'
+        return f'file for content -{self.content.title}'
+
