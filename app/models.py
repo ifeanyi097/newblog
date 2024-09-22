7f82ab2a4c21d0ebbf34d15f0e3ae1dcec165123
ifeanyi <ifeanyiogu097@gmail.com>
2024-09-22 00:50:05 +0000

    models

--- app/models.py
+++ app/models.py
@@ -5,11 +5,11 @@ class Content(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return f'content -{self.pk}'
+        return f'content -{self}'
 
 class Gallery(models.Model):
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
     file = models.FileField(upload_to='files')
 
     def __str__(self):
-        return f'file for content -{self.content.pk}'
+        return f'file for content -{self.content}'
